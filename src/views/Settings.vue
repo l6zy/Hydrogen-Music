f4362d536bab50601013baf529587ee1139e74da
Kaidesuyo <1208875929@qq.com>
2023-02-12 21:28:11 +0800

    检测更新功能测试

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -405,7 +405,7 @@
             <div class="app-icon">
                 <img src="../assets/icon/icon.ico" alt="">
             </div>
-            <div class="version">V0.2.0</div>
+            <div class="version">V0.2.1</div>
             <div class="app-author" @click="toGithub()">Author: Kaidesuyo</div>
         </div>
     </div>
